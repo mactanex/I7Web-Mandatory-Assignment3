8c50be6d2322b145c0a443ef06f15990b4ade82b
Malthe Dalgaard Jensen <malthedj@hotmail.com>
2019-12-09 18:22:00 +0100

    u wot mate

---
+++ dual-n-back/src/login.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useFormik } from 'formik';
+
+export function LoginForm() {
+    // Pass the useFormik() hook initial form values and a submit function that will
+    // be called when the form is submitted
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="email">Email Address</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
